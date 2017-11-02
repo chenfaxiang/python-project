48b7f1e1e79e9d2b2b29a86e7c4ca5777394f0e0
1061706520@qq.com <1061706520@qq.com>
2017-11-02 17:55:28 +0800

    增加类

--- one.py
+++ one.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+' a *.py module '
+__author__ = 'ChenFaxiang'
+
 print 'hello word，这就是来测试中文的。'
 
 print 'hello,%s' % '格式化字符串'
@@ -16,6 +19,7 @@ print '一共有同学：%d名' % len(allClassmateName)
 tup = (1,)
 print 'tuple数据类型在定义一个数据时必须加一个逗号，否则就会被当做括号1处理 %s' % tup
 
+
 def power(x, n):
     s = 1
     while n > 0:
@@ -29,3 +33,52 @@ print a
 
 L = ['Hello', 'World', 18, 'Apple', None]
 print [s.lower() for s in L if isinstance(s, str)]
+
+import sys
+
+
+def test():
+    args = sys.argv
+    if len(args) == 1:
+        print 'Hello, world!'
+    elif len(args) == 2:
+        print 'Hello, %s!' % args[1]
+    else:
+        print 'Too many arguments!'
+
+if __name__ == '__main__':
+    test()
+
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+newStu = Student('chen', 100)
+print 'this is the Student Class name: %s' % newStu.name
+print 'this is the Student Class score: %s' % newStu.score
+
+
+class Fib(object):
+    def __init__(self):
+        # 初始化两个计数器a，b
+        self.a, self.b = 0, 1
+
+    def __iter__(self):
+        # 实例本身就是迭代对象，故返回自己
+        return self
+
+    def next(self):
+        # 计算下一个值
+        self.a, self.b = self.b, self.a + self.b
+
+        # 退出循环的条件
+        if self.a > 100000:
+            raise StopIteration()
+        # 返回下一个值
+        return self.a
+
+for n in Fib():
+    print n
